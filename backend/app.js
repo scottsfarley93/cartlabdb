04b485088faff15b2e1bd702fff0185b9ee23810
Scott Farley <sfarley2@wisc.edu>
2016-07-15 18:35:21 -0500

    Query Totally Working

--- backend/app.js
+++ backend/app.js
@@ -100,14 +100,14 @@ app.post('/upload', function(req, res){
       db.one("INSERT INTO Resources Values(default, $1, $2, $3, (select categoryid from categories where lower(categorytext) = $4), $5, $6, $7, $8, $9, (select mediaTypeID from MediaTypes where lower(mimetype) = $10), $11, $12, $13, FALSE, default) returning resourceid;", vals)
       .then(function(data){
           //now add authorships
-          var rowid = data['resourceid']
-          console.log("Inserted resource.  ID is " + rowid)
+          var resourceID = data['resourceid']
+          console.log("Inserted resource.  ID is " + resourceID)
           sql = ""
           vals = []
           for (var i=0; i<authors.length; i++){
             j = i * 4 + 1
             sql += "INSERT INTO Authorship VALUES (default, $" + j + ",$" + (j + 1) + ",$" + (j + 2) + ",$" + (j + 3) + ", default); "
-            vals.push(rowid, authors[i]['first'], authors[i]['last'], authors[i]['middle'])
+            vals.push(resourceID, authors[i]['first'], authors[i]['last'], authors[i]['middle'])
           }
           db.none(sql, vals)
             .then(function(){
@@ -125,7 +125,7 @@ app.post('/upload', function(req, res){
                       j = i * 2 + 1
                       tagValue = tags[i].replace(/ /g,''); //remove extra whitespace
                       sql += "INSERT INTO tags VALUES (default, $" + j + ", $" + (j + 1) + ", default); "
-                      vals.push(rowid, tagValue)
+                      vals.push(resourceID, tagValue)
                   }
                   console.log(sql)
                   db.none(sql, vals)
@@ -252,8 +252,10 @@ function insertCitation(citation, resourceID, db){
   place = null
   typeOfReference = null
   doi = null
-  db.none("INSERT INTO objectreferences values(default, $1, $2, $3, $4, $5, $6, $7,$8, $9, $10, $11, $12, $13 )", [resourceID, authorString,
-        title, journal, place, volume, number, pages, year, publisher, doi, typeOfReference, raw])
+  values = [resourceID, authorString, title, journal, place, volume, number, pages, year, publisher, doi, typeOfReference, raw]
+  console.log(values)
+  db.none("INSERT INTO objectreferences values(default, $1, $2, $3, $4, $5, $6, $7,$8, $9, $10, $11, $12, $13 )",
+  values)
         .then(function(){
           console.log("Inserted reference")
         })
@@ -531,17 +533,19 @@ app.get("/search", function(req, res){
   journal = req.query.journal
   title = req.query.title
   category = req.query.category
-  //format tags into regex
+  refQ = req.query.refq //query any reference
   tags = req.query.category
   offset = req.query.offset
   limit = +req.query.limit
   offset = +req.query.offset
+  pubYear = req.query.pubYear //year of publication
+  refOnly = req.query.refOnly //only include objects with references
 
 
 
   //make sure things are correctly defined or undefined
   if ((isNaN(limit)) || (limit === undefined)){
-    limit = 10;
+    limit = 100;
   }
   if ((isNaN(offset)) || (offset === undefined)){
     offset = 0;
@@ -550,6 +554,7 @@ app.get("/search", function(req, res){
   if(sortBy != undefined){
     sortby = sortBy.lower()
   }
+  //format tags into regex
   if (tags != undefined){
     tagstring = ""
     for (var  i=0; i< tags.length; i++){
@@ -565,8 +570,11 @@ app.get("/search", function(req, res){
     case 'title':
       sortField = 'resources.resourcetitle';
       break;
-    case 'date':
-      sortField = 'resources.resourcedate';
+    case 'upload':
+      sortField = 'resources.modified';
+      break
+    case 'created':
+      sortField = 'resources.resourcedate'
       break
     case 'author':
       sortField = 'authorships.authorlast';
@@ -574,16 +582,27 @@ app.get("/search", function(req, res){
     default:
       sortField = 'resources.resourceid'
   }
-  console.log(sortField)
+  //sort out the min/max
+  maxResource = offset + limit
 
   //build the query
   db = createConnection()
 
-  sql = "SELECT * FROM Resources \
+  sql = "SELECT\
+          resources.resourceid, resources.resourcename, resources.resourcetitle, \
+          resources.resourcedate, resources.resourcedescription, categories.categorytext, \
+          resources.objectreference, resources.objectsize, authorship.authorshipid, authorship.authorfirst, authorship.authorlast, \
+          authorship.authormiddle, tags.tagtext, objectreferences.referenceid, objectreferences.authors, \
+          objectreferences.title, objectreferences.journal, objectreferences.place, objectreferences.volume, \
+          objectreferences.issue, objectreferences.pages, objectreferences.pubyear, objectreferences.publisher, \
+          objectreferences.doi, objectreferences.typeofreference, objectreferences.rawref, mediatypes.mimetype, mediatypes.description \
+          FROM (SELECT DENSE_RANK() OVER (ORDER BY resources.resourceid) AS dr, resources.*\
+             FROM resources) resources  \
       	LEFT OUTER JOIN Authorship on Authorship.resourceid = resources.resourceid \
-      	LEFT outer join ObjectReferences on ObjectReferences.resourceid = resources.resourceid \
-      	LEFT OUTER JOIN Tags on Tags.resourceid = resources.resourceid \
+        LEFT OUTER JOIN Tags on Tags.resourceid = resources.resourceid \
+      	LEFT OUTER JOIN ObjectReferences on ObjectReferences.resourceid = resources.resourceid \
       	INNER JOIN Categories on Categories.categoryid = Resources.resourcecategory \
+        INNER JOIN MediaTypes on Resources.ObjectType = MediaTypes.mediatypeid \
       WHERE \
         	1 = 1 \
           AND (($1 IS NULL OR lower(resourcetitle) LIKE '%' || lower($1) || '%') \
@@ -601,17 +620,127 @@ app.get("/search", function(req, res){
           AND ($8 IS NULL OR resourcedate >= $8)\
           AND (rejected = FALSE)\
           AND (embargostatus = FALSE) \
-      ORDER BY $9:value \
-      LIMIT $10:value \
-      OFFSET $11:value  \
+          AND (($9 IS NULL or lower(title) LIKE '%' || lower($9) || '%')\
+            OR ($9 IS NULL or lower(journal) LIKE '%' || lower($9) || '%')\
+            OR ($9 IS NULL or lower(authors) LIKE '%' || lower($9) || '%')\
+            OR ($9 IS NULL or lower(place) LIKE '%' || lower($9) || '%')\
+            OR ($9 IS NULL or lower(publisher) LIKE '%' || lower($9) || '%')\
+            OR ($9 IS NULL or lower(rawref) LIKE '%' || lower($9) || '%'))\
+          AND ($10 IS NULL or pubYear = $10)\
+          AND dr BETWEEN $12:value AND ($13:value)\
+      ORDER BY $11:value \
         ;"
   console.log(sql)
-  values =[q, author, category, journal, title, tagstring, maxdate, mindate, sortField, limit, offset]
+  values =[q, author, category, journal, title, tagstring, maxdate, mindate, refQ, pubYear, sortField, offset, maxResource]
   console.log(values)
   db.any(sql, values)
     .then(function(data){
-      res.json(data)
-    })
+      //now we need to convert straight rows to nested JSON
+      //structure is resource --> author(s) --> tag(s) -- > reference(s)
+      success = true
+      timestamp = new Date().toLocaleString()
+      theJSON = {timestamp: timestamp}
+      responses = {}
+      for (var i =0; i<data.length; i++){
+        //check if we have processed this one already
+        thisRow = data[i]
+        thisID = thisRow['resourceid']
+        console.log(thisRow)
+        if (responses[thisID] === undefined){
+          responses[thisID] = {
+            authors : [],
+            references : [],
+            tags : []
+          } //this is the base response, to which we add additional properties
+          //add the properties directly
+          responses[thisID]['resourceID'] = thisID
+          responses[thisID]['resourceTitle'] = thisRow['resourcetitle']
+          responses[thisID]['resourceCreationDate'] = thisRow['resourcedate']
+          responses[thisID]['resourceCategory'] = thisRow['categorytext']
+          responses[thisID]['resourceShortName'] = thisRow['resourcename']
+          responses[thisID]['resourceDescription'] = thisRow['resourcedescription']
+          responses[thisID]['fileReference'] = thisRow['objectreference']
+          responses[thisID]['mimetype'] = thisRow['mimetype']
+          responses[thisID]['fileDescription'] = thisRow['Description']
+          responses[thisID]['fileSize'] = thisRow['objectSize']
+          responses[thisID]['uploaderName'] = thisRow['uploaderName']
+        } //end new resource creation
+        //if its not new, we can add authors, tags, and references
+        //do this because it's a left join
+        authorFirst = thisRow['authorfirst']
+        authorMiddle = thisRow['authormiddle']
+        authorLast = thisRow['authorlast']
+        authorshipid = thisRow['authorshipid'] //this is just for reference purposes so that the object is IDed in the array
+        thisAuthor = {
+          'first' : authorFirst,
+          'last' : authorLast,
+          'middle' : authorMiddle,
+          'id' : authorshipid
+        }
+        if (!pluckByID(responses[thisID].authors, authorshipid, true)){
+          responses[thisID].authors.push(thisAuthor)
+        }
+        //now do tags
+        tagtext = thisRow['tagtext']
+        if ((tagtext != null) && (tagtext != undefined)){
+          tagtext = tagtext.toLowerCase()
+          if (responses[thisID].tags.indexOf(tagtext) == -1){
+            responses[thisID].tags.push(tagtext)
+          }
+        } // end of tag-if
+
+        //finally do references
+        thisRefID = thisRow['referenceid']
+        console.log(thisRefID)
+        if ((thisRefID != null) && (thisRefID != undefined)){
+          //we definitely have a reference then, so go ahead and parse it
+          //this could be easily modified to return bibtex
+          refTitle = thisRow['title']
+          refAuthors = thisRow['authors']
+          refJournal = thisRow['journal']
+          refPlace = thisRow['place']
+          refVolume = thisRow['volume']
+          refPages = thisRow['pages']
+          refPubYear =  thisRow['pubyear']
+          refPub = thisRow['publisher']
+          refDOI = thisRow['doi']
+          refType = thisRow['typeofreference']
+          refRaw = thisRow['rawref']
+          ref  = {
+            'title' : refTitle,
+            'authors': refAuthors,
+            'journal' : refJournal,
+            'place' : refPlace,
+            'volume' : refVolume,
+            'pages' : refPages,
+            'year' : refPubYear,
+            'publisher' : refPub,
+            'doi' : refDOI,
+            'type' : refType,
+            'string' : refRaw,
+            'id' : thisRefID
+          }
+          if (!pluckByID(responses[thisID].references, thisRefID,  true)){
+            responses[thisID].references.push(ref)
+          }else{
+            console.log("Reference exists.")
+          }
+          ref = {}
+        }//end of ref-if
+      } // end loop
+
+      //now convert the object to an array for transmit
+      responseList = []
+      for(key in responses){
+        response = responses[key]
+        responseList.push(response) //this is an individual resource object
+      }
+
+      //now send it back to the client
+      theJSON['data'] = responseList
+      res.json(theJSON)
+
+    }) //end db success function
     .catch(function(err){
       res.json(err)
     })
@@ -621,5 +750,27 @@ app.get("/", function(req, res){
   db = createConnection();
   res.json({message: "App is up and running.", serverStatus: "Running", dbStatus: "Running"})
 })
+
+function pluckByID(inArr, id, exists)
+{
+    for (i = 0; i < inArr.length; i++ )
+    {
+        if (inArr[i].id == id)
+        {
+            return (exists === true) ? true : inArr[i];
+        }
+    }
+}
+
+function pluckByName(inArr, id, exists)
+{
+    for (i = 0; i < inArr.length; i++ )
+    {
+        if (inArr[i].name == name)
+        {
+            return (exists === true) ? true : inArr[i];
+        }
+    }
+}
 
 app.listen(PORT);
